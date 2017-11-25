54955c174d5a5c2025bacf312eba3b5b9ae46953
Jan Hoon <janhoon@Jans-MacBook-Pro.local>
2017-11-25 15:14:30 +0200

    added firebase connection for user auth

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -1,8 +1,19 @@
 var express = require('express'),
-  router = express();
+  router = express(),
+  firebase = require('firebase');
 
+var config = {
+  apiKey: "AIzaSyAwhVc2xm2sVPJJ5WwzYB_5xY4zVsBU_sk",
+  authDomain: "social-tech-app.firebaseapp.com",
+  databaseURL: "https://social-tech-app.firebaseio.com",
+  projectId: "social-tech-app",
+  storageBucket: "social-tech-app.appspot.com",
+  messagingSenderId: "1063863987369"
+};
 
 module.exports = function (app) {
+  firebase.initializeApp(config);
+  console.log("Connection to Firebase established")
   app.use('/', router);
 };
 

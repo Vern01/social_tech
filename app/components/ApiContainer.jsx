fefd2232f73a130f5ebf1ccae7a10ee9b34e4e3a
Jan Hoon <janhoon@Jans-MacBook-Pro.local>
2018-01-03 13:17:56 +0200

    added logins with 3rd parties

--- app/components/ApiContainer.jsx
+++ app/components/ApiContainer.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
+import React from 'react';
+import {connect} from 'react-redux';
+
+import * as actions from 'actions';
 
 class ApiContainer extends React.Component {
+	constructor() {
+		super();
+
+		this.handleLogin = this.handleLogin.bind(this);
+	}
+
+	handleLogin(method) {
+		var {dispatch} = this.props;
+
+		switch (method) {
+			case 'google':
+				dispatch(actions.googleLogin());
+				break;
+			case 'facebook':
+				dispatch(actions.facebookLogin());
+				break;
+			case 'twitter':
+				dispatch(actions.twitterLogin());
+				break;
+			default:
+				console.error('Invalid login API');
+		}
+	}
+
     render() {
         return (
             <div className="api-container">
                 <div className="api-box">
-                    <img src="" alt="" className="api-image"/>
+                    <img src="" alt="" className="api-image" onClick={() => this.handleLogin('google')}/>
                 </div>
                 <div className="api-box">
-                    <img src="" alt="" className="api-image"/>
+                    <img src="" alt="" className="api-image" onClick={() => this.handleLogin('facebook')}/>
                 </div>
                 <div className="api-box">
-                    <img src="" alt="" className="api-image"/>
+                    <img src="" alt="" className="api-image" onClick={() => this.handleLogin('twitter')}/>
                 </div>
             </div>
         );
     }
 }
 
-export default ApiContainer;
+export default connect()(ApiContainer);

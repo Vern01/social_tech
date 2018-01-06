e4b9d3e2a8f57da8a912bba96af71a4ca54d0cf4
Vern01 <verdunw2@gmail.com>
2018-01-06 17:40:02 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,39 @@
 module.exports = {
 	entry: './app/app.jsx',
-	output: {
-		path: __dirname,
-		filename: './public/bundle.js'
-	},
-	resolve: {
-		modules: [__dirname, 'node_modules'],
-		alias: {
-			DesignFormBuilder: 'app/builder/DesignFormBuilder',
-			FormBuilder: 'app/builder/FormBuilder',
-			RecursivePageBuilder: 'app/builder/RecursivePageBuilder',
-			BaselineContainer: 'app/components/containers/BaselineContainer',
-			Body: 'app/components/containers/Body',
-			ButtonsContainer: 'app/components/containers/ButtonsContainer',
-			CenterContainer: 'app/components/containers/CenterContainer',
-			Fill: 'app/components/containers/Fill',
-			FillContainer: 'app/components/containers/FillContainer',
-			FillNone: 'app/components/containers/FillNone',
-            HDivider: 'app/components/containers/HDivider',
-			HorizontalContainer: 'app/components/containers/HorizontalContainer',
-			ListContainer: 'app/components/containers/ListContainer',
-			ModalContainer: 'app/components/containers/ModalContainer',
-			SwitchButtonContainer: 'app/components/containers/SwitchButtonContainer',
-			ApiContainer: 'app/components/ApiContainer',
-			DesignItem: 'app/components/DesignItem',
-			DesignListGroup: 'app/components/DesignListGroup',
-			FeatureGroup: 'app/components/FeatureGroup',
-			FormPasswordInput: 'app/components/FormPasswordInput',
-			FormSubmit: 'app/components/FormSubmit',
-			FormTextArea: 'app/components/FormTextArea',
-			FormTextInput: 'app/components/FormTextInput',
-            H1: 'app/components/H1',
-            H2: 'app/components/H2',
-            Header: 'app/components/Header',
-			IconText: 'app/components/IconText',
-			IconTextBox: 'app/components/IconTextBox',
-			ImageGroup: 'app/components/ImageGroup',
-			Input: 'app/components/Input',
-			InputArea: 'app/components/InputArea',
-			InputLabel:'app/components/InputLabel',
-			Intro: 'app/components/Intro',
-			LargeImage: 'app/components/LargeImage',
-			P1: 'app/components/P1',
-			Paragraph: 'app/components/Paragraph',
-			RoundBtn: 'app/components/RoundBtn',
-			RoundBtnGroup: 'app/components/RoundBtnGroup',
-			SearchBar: 'app/components/SearchBar',
-			SwitchButton: 'app/components/SwitchButton',
-			SwitchButtonGroup: 'app/components/SwitchButtonGroup',
-			TwoItem: 'app/components/TwoItem',
-			TwoListGroup: 'app/components/TwoListGroup',
-			VerticalMenu: 'app/components/VerticalMenu',
-            About: 'app/pages/About',
-			CustomerLogin: 'app/pages/CustomerLogin',
-			CustomerRegister: 'app/pages/CustomerRegister',
-            Customize: 'app/pages/Customize',
-			Dashboard: 'app/pages/Dashboard',
-			DesignerDashboard: 'app/pages/DesignerDashboard',
-            Home: 'app/pages/Home',
-			MakerDashboard: 'app/pages/MakerDashboard',
-			NewDesign: 'app/pages/NewDesign',
-			PickMaker: 'app/pages/PickMaker',
-			ProductView: 'app/pages/ProductView',
-			RegisterDesigner: 'app/pages/RegisterDesigner',
-			RegisterMaker: 'app/pages/RegisterMaker',
-			Search:'app/pages/Search',
-			Test: 'app/pages/Test',
-			BaseCss: 'app/styles/base'
-		},
-		extensions: ['.js', '.jsx', '.css']
-	},
-	module: {
-		loaders: [
-			{
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015', 'stage-0']
-				},
-				test: /\.jsx?$/,
-				exclude: /(node_modules|bower_components)/
-			}
-		]
-	},
+    output: {
+	    path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+	    modules: [
+	        __dirname,
+            'node_modules',
+            'app/components',
+            'app/components/containers',
+            'app/builder',
+            'app/pages'
+        ],
+        alias: {
+	        BaseCss: 'app/styles/base',
+            //Redux:
+            reducers: 'app/reducers/reducers',
+            actions: 'app/actions/actions',
+            configureStore: 'app/store/configureStore',
+            reactHistory: 'app/history/history.jsx'
+        },
+        extensions: ['.js', '.jsx', '.scss']
+    },
+    module: {
+	    loaders: [
+	        {
+	            loader: 'babel-loader',
+                query: {
+	                presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+	        }
+        ]
+    },
     devtool: 'cheap-module-eval-source-map'
 };
